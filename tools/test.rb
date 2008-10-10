d93b97e4617540be69b66360e09a91c646b11d51
Trevor Fountain <doches@gmail.com>
2008-10-10 17:18:14 +0100

    Added sane option handling, a --format option to explicitly specify XML or treebank input, and some mild cleverness to detect input type if left unspecified.

--- tools/test.rb
+++ tools/test.rb
@@ -1,11 +1,49 @@
 #!/usr/bin/ruby
 
+require 'optparse'
+require 'ostruct'
+
+options = OpenStruct.new
+options.format=:auto
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: ./test.rb path/to/input.xml [options]"
+  opts.banner = <<USE
+test.rb featurizes preprocessed input, which can be either SalsaTiger XML
+preprocessed by Shalmaneser, or Treebank bracketed parse trees, one tree per
+line.
+
+Usage: ./test.rb path/to/feature_extractor.rb path/to/input [OPTIONS]
+  OR
+Usage: cat input | ./test.rb path/to/feature_extractor.rb [OPTIONS]
+
+OPTIONS
+USE
+  
+  opts.on("--format [TYPE]",[:xml, :treebank,:auto],
+          "Select input type (xml, treebank,auto)",
+          "  Default is AUTO, which will treat input from STDIN and with a .xml",
+          "  file extension as XML, and all other input as TREEBANK") do |format|
+    options.format = format
+  end
+   
+  opts.on_tail("-h","--help","Show this help text") do
+    puts opts
+    exit
+  end
+end
+
+begin
+  optparser.parse!
+rescue 
+  ; # We were given invalid options: run with defaults and see what happens!
+end
+
 # Get parse tree(s)
 input = ""
 sentences = nil
 if not ARGV[1].nil?
   IO.foreach(ARGV[1]) { |l| input += l }
-  if ARGV[1] =~ /\.xml/
+  if (options.format == :auto and ARGV[1] =~ /\.xml$/) or options.format == :xml
     require 'lib/xml2pt'
     sentences = XMLSentence.xml2parsetree(input)
   else
@@ -13,9 +51,29 @@ if not ARGV[1].nil?
     sentences = BracketSentence.lines2parsetree(input.split("\n"))
   end
 else
-  require 'lib/xml2pt'
   STDIN.each_line { |l| input += l }
-  sentences = XMLSentence.xml2parsetree(input)
+  
+  if options.format == :xml
+    require 'lib/xml2pt'
+    sentences = XMLSentence.xml2parsetree(input)
+  elsif options.format == :treebank
+    require 'lib/bracket2pt'
+    sentences = BracketSentence.lines2parsetree(input.split("\n"))
+  elsif options.format == :auto
+    begin
+      require 'lib/xml2pt'
+      sentences = XMLSentence.xml2parsetree(input)
+      throw :try_bracket if sentences.empty?
+    rescue 
+      begin
+        require 'lib/bracket2pt'
+        sentences = BracketSentence.lines2parsetree(input.split("\n"))
+      rescue
+        STDERR.print "Unknown input format -- FDL only understands SalsaTigerXML and treebank-style trees, one tree per line\n"
+        exit(1)
+      end
+    end
+  end
 end
 
 # Get features
