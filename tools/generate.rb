3cf2da635f1c2bd290010055b628a93856bcd847
Trevor Fountain <doches@gmail.com>
2008-10-10 17:16:28 +0100

    Added sane option handling, an EDITOR option (replace the old -noedit hack), and usage information.

--- tools/generate.rb
+++ tools/generate.rb
@@ -1,7 +1,40 @@
 #!/usr/bin/env ruby
+
+=begin
+
+generate.rb is a convenience tool that chains together the tools fdl2tree.rb
+and tree2rb.rb.
+
+Trevor Fountain, 10 October 2008
+
+=end
+
+require 'optparse'
+require 'ostruct'
+
+options = OpenStruct.new
+options.editor = nil
+optparser = OptionParser.new do |opts|
+  opts.banner = "Usage: ./generate.rb path/to/file.fdl [options]"
+  
+  opts.on("-e", "--edit [EDITOR]",
+          "View the output in EDITOR",
+          "  (defaults to $EDITOR if no editor is supplied)") do |editor|
+    options.editor = editor || ENV['EDITOR']
+  end
+  
+  opts.on_tail("-h","--help","Show this help text") do
+    puts opts
+    exit
+  end
+end
+
+optparser.parse!
+
 feature = ARGV[0]
 if feature.nil?
-  print "Usage: ./generate.rb <featurename> [-noedit]\n"
+  puts "No feature file specified!"
+  puts optparser
   exit(1)
 end
 
@@ -9,12 +42,9 @@ if feature[feature.size-4..feature.size] == ".fdl"
   feature = feature[0..feature.size-5]
 end
 
-editor = ENV['EDITOR']
-editor ||= "nano"
-
 output = %x{cat #{feature}.fdl | parser/parser}
 # | ./fdl2tree.rb | ./tree2rb.rb > #{feature}.rb`
 if $?.exitstatus == 0
   output = %x{echo "#{output}" | ./fdl2tree.rb | ./tree2rb.rb > #{feature}.rb}
-  exec("#{editor} #{feature}.rb") if ARGV[1].nil? and $?.exitstatus == 0
+  exec("#{options.editor} #{feature}.rb") if not options.editor.nil? and $?.exitstatus == 0
 end
